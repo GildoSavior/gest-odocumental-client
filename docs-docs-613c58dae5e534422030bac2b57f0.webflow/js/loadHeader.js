31f23eb55225fb0eb41e964177b711af395c9df8
Gildo Salvador <gildosalva.pe@gmail.com>
2025-04-15 15:59:33 +0100

    Add all year in select year

--- docs-docs-613c58dae5e534422030bac2b57f0.webflow/js/loadHeader.js
+++ docs-docs-613c58dae5e534422030bac2b57f0.webflow/js/loadHeader.js
@@ -5,6 +5,26 @@ document.addEventListener("DOMContentLoaded", function () {
     const subFolderName = localStorage.getItem("selectedSubFolderName") || "Subpasta desconhecida";
     const breadcrumb = document.querySelector(".text-block-113");
 
+    const dropdownList = document.querySelector(".dropdown-list-4");
+
+    if (dropdownList) {
+        dropdownList.innerHTML = ""; // Limpa os links existentes
+
+        for (let year = 2025; year >= 2000; year--){
+            const link = document.createElement("a");
+            link.href = `pastas/main-pastas-do-ano.html?ano=${year}`; // Ajuste conforme sua estrutura de URLs
+            link.className = "droplink-anos w-dropdown-link";
+            link.textContent = year;
+
+            // Se quiser marcar o ano atual como "ativo":
+            if (year === new Date().getFullYear()) {
+                link.classList.add("w--current");
+                link.href = "main-dashboard.html"; // Ex: ano atual leva ao dashboard principal
+            }
+
+            dropdownList.appendChild(link);
+        }
+    }
     if (!breadcrumb) return;
 
     if (window.location.href.includes("inside-sub-folder")) {
